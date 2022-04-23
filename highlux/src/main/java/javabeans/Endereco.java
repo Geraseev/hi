fb76c6bf3014344039cf5cea399a9eef2d8e1004
Lara Geraseev <68522627+Geraseev@users.noreply.github.com>
2022-04-23 14:56:49 -0300

    Create Endereco.java

---
+++ highlux/src/main/java/javabeans/Endereco.java
@@ -0,0 +1,81 @@
+package javabeans;
+
+/**
+ *
+ * @author lgera
+ */
+public class Endereco {
+    private int idendereco;
+    private String logradouro;
+    private int numero;
+    private String bairro;
+    private String cidade;
+    private String estado;
+    private int cep;
+    private String complemento;
+
+    public int getIdendereco() {
+        return idendereco;
+    }
+
+    public void setIdendereco(int idendereco) {
+        this.idendereco = idendereco;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public int getCep() {
+        return cep;
+    }
+
+    public void setCep(int cep) {
+        this.cep = cep;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+    
+}
